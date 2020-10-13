ad72a80108a576f476df3867aa731a1a9c97e88d
Lacji <jerome.auffret.ja@gmail.com>
2020-10-13 19:26:12 +1100

    Add logout method to demo2 controller

--- src/apps/demo2/controllers/IndexController.php
+++ src/apps/demo2/controllers/IndexController.php
@@ -13,4 +13,15 @@ class IndexController extends BaseController
      */
     public function indexAction() {}
 
+    /**
+     *
+     */
+    public function logoutAction()
+    {
+        $this->session->destroy();
+        $this->response->redirect('auth/login');
+
+        return $this->response;
+    }
+
 }
