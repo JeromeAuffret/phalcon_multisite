a25eb09516e440dc7e160ffe4655a0b4e6765a0a
Lacji <jerome.auffret.ja@gmail.com>
2020-10-17 20:15:25 +1100

    Common Application should register default services

--- src/common/Application.php
+++ src/common/Application.php
@@ -2,17 +2,6 @@
 
 namespace Common;
 
-use Phalcon\Di\DiInterface;
 use Provider\ApplicationProvider;
 
-class Application extends ApplicationProvider
-{
-    /**
-     * @Override
-     *
-     * Disabled specific application services in common
-     *
-     * @param DiInterface|null $container
-     */
-    public function registerServices(DiInterface $container = null) {}
-}
+class Application extends ApplicationProvider {}
