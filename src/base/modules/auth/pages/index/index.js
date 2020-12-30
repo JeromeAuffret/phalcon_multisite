90f2a2f458c716c9c03650ea7b7268f0dc3213f8
Jerome <jerome.auffret.ja@gmail.com>
2020-12-30 20:22:57 +1100

    Remove unused store import

--- src/base/modules/auth/pages/index/index.js
+++ src/base/modules/auth/pages/index/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 
 import index from './Index.vue';
-import store from '@/base/store';
 import Login from "@/base/modules/auth/components/Login";
 
 const vm = new Vue({
-  store,
   el: '#auth_index',
   components: {
     'welcome': index,
